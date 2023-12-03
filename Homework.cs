d94a446c82e098f2db8a32e86931b9d350830773
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-12-03 16:51:31 +0500

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = "asdyyhiu213434598/'sdfi!ihgfd@ad";
+            StringBuilder result = new StringBuilder();
+            string rez = string.Empty;
+            string symbols="!@#$%^&*()-+[]{}<>/'";
+            foreach(char item in start)
+            {
+                foreach(char c in symbols)
+                {
+                    if(item == c)
+                    {
+                        result.Append(c);
+                    }
+                }               
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
